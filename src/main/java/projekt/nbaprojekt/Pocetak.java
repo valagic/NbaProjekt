77df15a07d10ad8eb5e11d55ef92f684f0189e4c
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-08-19 13:26:40 +0200

    kreiranje prozora(Zanimljivosti i informacije) - potrebno doraditi

--- src/main/java/projekt/nbaprojekt/Pocetak.java
+++ src/main/java/projekt/nbaprojekt/Pocetak.java
@@ -45,28 +45,61 @@ public class Pocetak {
                         case 4: popisUtakmicaProslaGodina();
                         break;
                             case 5: informacijeOLigi();
-                            break;
+                            
         }        
     }
 
     private void zanimljivostiOLigi() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("");
+        System.out.println("The National Basketball Association (NBA) is a professional basketball league in North America.");
+        System.out.println("The league is composed of 30 teams (29 in the United States and 1 in Canada) and is one of the four major professional"
+                + " sports leagues in the United States and Canada.");
+        System.out.println("It is the premier men's professional basketball league in the world.");
+        System.out.println("The league was founded in New York City on June 6, 1946, ");
+        System.out.println("as the Basketball Association of America (BAA).[2] ");
+        System.out.println("It changed its name to the National Basketball Association on August 3, 1949, ");
+        System.out.println("after merging with the competing National Basketball League (NBL).[3] ");
+        System.out.println("The NBA's regular season runs from October to April, with each team playing 82 games. ");
+        System.out.println("The league's playoff tournament extends into June. As of 2020, ");
+        System.out.println("NBA players are the world's best paid athletes by average annual salary per player.");
+        System.out.println("");
+        if(Ulaz.daIliNe("Ako zelite saznati vise o programu upisite da u suprotnom ne", "Nisi upisao da")) {
+           System.out.println("");
+           pocetniEkran();
+        }else {
+            System.out.println("");
+            pocetniEkran();
+        }
+            
+            
     }
 
     private void popisEkipa() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     private void popisIgraca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     private void popisUtakmicaProslaGodina() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     private void informacijeOLigi() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Liga je podjeljena na 2 konferencije, istocnu i zapadnu.");
+        System.out.println("U istocnoj konferenciji se nalazi 3 divizije.");
+        System.out.println("U zapadanoj konferenciji se nalazi 3 divizije.");
+        System.out.println("Svaka divizija ima po nekoliko ekipa");
+        System.out.println("Ako te zanima u kojoj se diviziji nalazi koja ekipa klikni na ovaj link");
+        System.out.println("");
+        if(Ulaz.daIliNe("Ako zelite saznati vise o Ligi upisite da, u suprotnom vas vracamo na pocetni ekran", "Niste upisali da")) {
+            System.out.println("");
+            pocetniEkran();
+        }else {
+            System.out.println("");
+            pocetniEkran();
+        }
     }
     
 }
--- src/main/java/projekt/nbaprojekt/Ulaz.java
+++ src/main/java/projekt/nbaprojekt/Ulaz.java
@@ -5,11 +5,11 @@
  */
 package projekt.nbaprojekt;
 import java.util.Scanner;
+import java.util.*;
+import javax.swing.JOptionPane;
 
 
 public class Ulaz {
-
-    public static Scanner scanner;
 
    public static int ucitajCijeliBroj (String poruka, String greska, int min, int max) {
 		int broj = 0;
@@ -17,8 +17,8 @@ public class Ulaz {
 			System.out.println(poruka);
 			try {
 
-				broj = Integer.parseInt(
-						scanner.nextLine());
+				broj = Integer.parseInt(JOptionPane.showInputDialog("unesi broj"));
+						
 				if (broj<min || broj>max) {
 					System.out.println("Broj ne smije biti manji od " + min
 							+ " ili ve�i od " + max);
@@ -30,5 +30,20 @@ public class Ulaz {
 			}
 		}
 		return broj;
+	}
+   
+   public static  boolean daIliNe (String poruka, String greska) {
+		String upit = "";
+		while(true) {
+			System.out.println(poruka);
+			upit = JOptionPane.showInputDialog("ako zelis saznati vise upisi da, u suprotnom ne");
+			if(upit.trim().toLowerCase().equals("da")) {
+				return true;
+			}
+			if(upit.trim().toLowerCase().equals("ne")) {
+				return false;
+			}
+			System.out.println(greska + "Molimo unesite da ili ne");
+		}
 	}
 }
